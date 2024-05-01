639fae989ea65174e748602e720eff98d7db4b71
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-05-01 17:31:11 +0000

    set up functions for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
+function runGame(){
 
+}
+
+function checkAwnser(){
+
+
+}
+function calculateCorrectAnswer(){
+
+
+}
+function incrementScore(){
+
+
+}
+function incrementWrongAnswer(){
+
+
+}
+function displayAdditionQuestion(){
+
+
+}
+function displaySubstractQuestion(){
+
+
+}
+function displayMultiplyQuestion(){
+
+    
+}
